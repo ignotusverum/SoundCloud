5d2647442ebedb7e0c3d8898dffe0e42ea5fc4a5
Vlad Z <vladislavz1991@gmail.com>
2017-02-20 15:04:05 -0500

    clearing AppDelegate

---
+++ SoundCloud/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  SoundCloud
+//
+//  Created by Vladislav Zagorodnyuk on 2/20/17.
+//  Copyright © 2017 VZ. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
